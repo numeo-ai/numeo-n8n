166eaa823fdd265e6dee8477f2786a28beea8c7a
Dana Lee <dana@n8n.io>
2024-12-17 10:00:03 +0100

    Cleanup import order for Postgres util tests

--- packages/nodes-base/nodes/Postgres/test/v2/utils.test.ts
+++ packages/nodes-base/nodes/Postgres/test/v2/utils.test.ts
@@ -1,5 +1,7 @@
 import type { IDataObject, INode } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
+
+import type { ColumnInfo } from '../../v2/helpers/interfaces';
 import {
 	addSortRules,
 	addReturning,
@@ -13,7 +15,6 @@ import {
 	convertArraysToPostgresFormat,
 	isJSON,
 } from '../../v2/helpers/utils';
-import type { ColumnInfo } from '../../v2/helpers/interfaces';
 
 const node: INode = {
 	id: '1',
